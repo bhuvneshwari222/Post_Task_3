4786559b10eca5b1b9095a3df82ded0ecf03c3bf
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-05-30 17:00:36 +0530

    Post_Task_3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostDashboardComponent } from './shared/components/post-dashboard/post-dashboard.component';
+import { PostFormComponent } from './shared/components/post-form/post-form.component';
+import { PostCardComponent } from './shared/components/post-card/post-card.component';
+import { GetConfirmComponent } from './shared/components/get-confirm/get-confirm.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './shared/materials/module.materials';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostDashboardComponent,
+    PostFormComponent,
+    PostCardComponent,
+    GetConfirmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
